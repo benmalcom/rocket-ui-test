5aae659933c95589f6d35598d1226a0e1f5d7016
Ikeji Benjamin <benjaminikeji@gmail.com>
2020-04-06 09:57:20 +0100

    Add Collapsible into Launch item to display launch details

--- src/components/Launch.jsx
+++ src/components/Launch.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import Collapsible from "./Collapsible";
 
-const Launch  = (props) => {
+const Launch = (props) => {
+
     const { launch } = props;
     return (
-      <li>
-        <h2> { launch.mission_name } </h2>
-        <div> Flight Number: { launch.flight_number } </div>
-      </li>
+        <li className="launch-item">
+            <Collapsible title={launch.mission_name}>
+                <table className="launch-info-table">
+                    <tbody>
+
+                    <tr>
+                        <td>
+                            <h4 className="col-header">Rocket Name</h4>
+                            <p className="col-info">{launch.rocket.rocket_name}</p>
+                        </td>
+                        <td>
+                            <h4 className="col-header">Rocket Type</h4>
+                            <p className="col-info">{launch.rocket.rocket_type}</p>
+                        </td>
+                        <td>
+                            <h4 className="col-header">Rocket ID</h4>
+                            <p className="col-info">{launch.rocket.rocket_id}</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={3}>
+                            <h4 className="col-header">More Details about {launch.mission_name}</h4>
+                            <p className="col-info">{launch.details || 'Not available'}</p>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </Collapsible>
+        </li>
     );
 };
 
