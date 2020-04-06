bf9f104a6084027ce0b8be0a2f9ba9cc8ed18ace
Ikeji Benjamin <benjaminikeji@gmail.com>
2020-04-06 13:09:26 +0100

    Update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,16 @@ module.exports = {
   "rules": {
     "spaced-comment": 0,
     "no-underscore-dangle": 0,
-    "trailing-comma": 0
-  }
+    "trailing-comma": 0,
+    "no-use-before-define": 0
+  },
+  "globals": {
+    "describe": true,
+    "it": true,
+    "expect": true,
+    "require": true,
+    "window": true,
+    "document": true,
+    "console": true
+  },
 };
